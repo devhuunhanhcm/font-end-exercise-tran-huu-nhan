4db65ac2c9e6c138ab0a7f2a289dcad75fbaeeed
siwnhan <nhandeptrai137@gmail.com>
2022-01-04 22:40:51 +0700

    responsive

--- index.js
+++ index.js
@@ -27,8 +27,6 @@ window.addEventListener('DOMContentLoaded',() => {
                   settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
                   }
                 },
                 {
